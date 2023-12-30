53f12e88e1a51ec716b8fe786b5e3646b0f1f5bf
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-12-30 21:48:30 +0900

    Update heroicons導入／ホーム実装中

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,23 +1,51 @@
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
+import { ROUTES } from "@/constants/routes";
+import { Cog6ToothIcon } from "@heroicons/react/24/solid";
 
 export default function Home() {
   return (
-    <div>
-      <div>header</div>
+    <div className="text-center">
+      <div className="text-right">
+        <Button variant="secondary">
+          <Cog6ToothIcon className="w-5 h-5 " />
+          設定
+        </Button>
+      </div>
       <div>
-        <div>title</div>
+        <h1>面接対策オンライン</h1>
         <div>
-          actions
-          <Button></Button>
+          <Button className="text-white">面接を開始</Button>
         </div>
       </div>
-      <div>
-        <p>利用規約</p>
-        <p>プライバシーポリシー</p>
-        <p>ライセンス</p>
-        <p>クレジット</p>
+      <div className="fixed bottom-4 left-4 right-4">
+        <div>
+          <Button
+            variant="link"
+            size="sm"
+            className="block ml-auto h-6 pr-0 text-white"
+          >
+            <Link href={ROUTES.ABOUT}>面接対策オンラインとは</Link>
+          </Button>
+          <Button
+            variant="link"
+            size="sm"
+            className="block ml-auto h-6 pr-0 text-white"
+          >
+            <Link href={ROUTES.PRIVACY}>プライバシーポリシー</Link>
+          </Button>
+          <Button
+            variant="link"
+            size="sm"
+            className="block ml-auto h-6 pr-0 text-white"
+          >
+            <Link href={ROUTES.TERM}>利用規約</Link>
+          </Button>
+        </div>
       </div>
-      <div>footer</div>
+      <p className="fixed bottom-4 left-4 right-4 text-center">
+        ©2023 Shunsuke Oba
+      </p>
     </div>
   );
 }
---
+++ src/constants/routes.ts
@@ -0,0 +1,10 @@
+export const ROUTES = {
+  HOME: "/home",
+  READY: "/ready",
+  INTERVIEW: "/interview",
+  SETTING: "/setting",
+  ABOUT: "/about",
+  TERM: "/term",
+  PRIVACY: "/privacy",
+  LICENSE: "/license",
+};
