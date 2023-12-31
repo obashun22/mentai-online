9477dbfd1d7d805f4ecda4b0896611a9e13c6f9a
Shusnuke Oba <obashun22@yahoo.co.jp>
2023-12-31 15:54:38 +0900

    Update settingsページ実装中

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -1,9 +1,20 @@
+import { Separator } from "@/components/ui/separator";
+
 export default function Settings() {
   return (
     <div className="p-4 h-screen">
       <div className="container mx-auto">
-        <div></div>
-        <div>main-content</div>
+        <div>
+          <h2 className="text-2xl font-bold tracking-tight">設定</h2>
+          {/* <p className="text-muted-foreground">
+            設定が変更できます。変更した設定はすぐに反映されます。
+          </p> */}
+        </div>
+        <Separator className="my-3" />
+        <div>
+          <div>nav</div>
+          <div>main-content</div>
+        </div>
       </div>
     </div>
   );
