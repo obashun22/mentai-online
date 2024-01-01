58bd902c6bca2dfd0845e8d0e2b77dc2ccc50ae3
Shusnuke Oba <obashun22@yahoo.co.jp>
2024-01-02 00:48:56 +0900

    Update ready実装中

--- src/app/ready/page.tsx
+++ src/app/ready/page.tsx
@@ -9,6 +9,7 @@ import { Card } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { WebRTCCam } from "@/utils/camera";
 import { useEffect } from "react";
+import { AspectRatio } from "@/components/ui/aspect-ratio";
 
 export default function Ready() {
   useEffect(() => {
@@ -17,16 +18,25 @@ export default function Ready() {
   }, []);
   return (
     <div className="p-4 h-screen flex justify-center items-center">
-      <Card className="w-full h-full lg:w-2/3 lg:h-2/3 p-4 flex">
+      <Card className="w-full h-full lg:w-2/3 lg:h-2/3 p-4 flex border-none shadow-none">
         <div className="w-1/2 flex">
-          <video
-            id="video"
-            autoPlay
-            playsInline
-            className="p-4"
-            width={1280}
-            height={720}
-          />
+          <div className="w-full flex justify-center items-center">
+            <div className="w-full">
+              <AspectRatio ratio={16 / 9} className="bg-muted rounded-lg m-8">
+                <video
+                  id="video"
+                  autoPlay
+                  playsInline
+                  className="aspect-video object-cover rounded-lg"
+                  width={1280}
+                  height={720}
+                />
+              </AspectRatio>
+              <p className="my-2 mx-8 text-muted-foreground text-xs">
+                ブラウザの設定でカメラの使用を許可してください。
+              </p>
+            </div>
+          </div>
           <Separator orientation="vertical" />
         </div>
         settings
